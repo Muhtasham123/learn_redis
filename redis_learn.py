247d892e360b4f94919f0774143be13bb8a475db
Muhtasham123 <muhtashamfarooqi433@gmail.com>
2026-08-21 00:03:18 +0500

    learned lists and sets usage

--- redis_learn.py
+++ redis_learn.py
@@ -5,6 +5,7 @@ import json
 # connect to redis
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
+r.flushdb()
 # simple string keys and value usage
 r.set("name", "Muhtasham")
 r.set("age", 21)
@@ -22,8 +23,27 @@ print(r.hget("user:1", "age"))
 r.hincrby("user:1", "age", 2)
 print(r.hgetall("user:1"))
 print(r.hexists("user:1", "name"))
+
 # list usage
+r.lpush("notifications:user:1", "New comment on your post")
+r.lpush("notifications:user:1", "Ali started following you")
+r.lpush("notifications:user:1", "Your post got 100 likes")
+
+r.rpop("notifications:user:1")
+print(r.lrange("notifications:user:1", 0, -1))
+
 # set usage
+r.sadd("post:42:likes", "user_1")
+r.sadd("post:42:likes", "user_2")
+r.sadd("post:42:likes", "user_3")
+r.sadd("post:42:likes", "user_1")
+
+print(r.smembers("post:42:likes"))
+
+r.sadd("friends:user:1", "Ali", "Hina", "sara")
+r.sadd("friends:user:2", "Ahmed", "Hina", "sara")
+
+print(r.sinter("friends:user:1", "friends:user:2"))
 # set with scores usage
 # json storage
 # some utility commands
